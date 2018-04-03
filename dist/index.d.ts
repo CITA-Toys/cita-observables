d32e6e7556631ec59a93482ed95c799185652c5c
Keith <keithwhisper@gmail.com>
2018-04-03 16:58:35 +0800

    update: update with cita-web3-plugin

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,17 @@
-import { Observable } from '@reactivex/rxjs';
-export declare const newBlockNumber$: Observable<{}>;
-export declare const blockByNumber$: (blockNumber: string) => Observable<{}>;
-export declare const blockByHash$: (blockHash: string) => Observable<{}>;
-export declare const newBlockByNumber$: any;
-export declare const peerCount$: Observable<{}>;
-export declare const multicastedNewBlockByNumber$: any;
-export declare const sendTransaction$: (signedData: any) => Observable<{}>;
-export declare const signTx$: (privKey: string, data: string) => Observable<any>;
+import { Observable, ReplaySubject } from '@reactivex/rxjs';
+export default class CITAObservables {
+    server: string;
+    interval: string | number;
+    CitaWeb3: any;
+    constructor({server, interval}: {
+        server: string;
+        interval?: string | number;
+    });
+    newBlockNumber$: (interval: any) => Observable<{}>;
+    blockByNumber$: (blockNumber: string) => Observable<{}>;
+    blockByHash$: (blockHash: string) => Observable<{}>;
+    newBlockByNumber$: (interval: any) => Observable<{}>;
+    peerCount$: (interval: any) => Observable<{}>;
+    multicastedNewBlockByNumber$: (interval: any) => ReplaySubject<{}>;
+    sendTransaction$: (signedData: any) => Observable<{}>;
+}
--- dist/index.js
+++ dist/index.js
@@ -3,37 +3,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var rxjs_1 = require("@reactivex/rxjs");
-var cita_web3_plugin_1 = __importDefault(require("cita-web3-plugin"));
-var Web3Ins = cita_web3_plugin_1.default();
-var CitaWeb3 = Web3Ins.CITA;
-var INTERVAL = process.env.NODE_ENV === 'production' ? 1000 : 10000;
-exports.newBlockNumber$ = rxjs_1.Observable.interval(INTERVAL).switchMap(function () {
-    return CitaWeb3.getBlockNumber();
-});
-exports.blockByNumber$ = function (blockNumber) {
-    return rxjs_1.Observable.fromPromise(CitaWeb3.getBlockByNumber({
-        quantity: blockNumber,
-        detailed: true,
-    }));
-};
-exports.blockByHash$ = function (blockHash) {
-    return rxjs_1.Observable.fromPromise(CitaWeb3.getBlockByHash({
-        hash: blockHash,
-        detailed: true,
-    }));
-};
-exports.newBlockByNumber$ = exports.newBlockNumber$
-    .distinct()
-    .switchMap(exports.blockByNumber$);
-exports.peerCount$ = rxjs_1.Observable.interval(INTERVAL).switchMap(function () {
-    return CitaWeb3.netPeerCount();
-});
-var newBlockByNumberSubject = new rxjs_1.ReplaySubject(10);
-exports.multicastedNewBlockByNumber$ = exports.newBlockByNumber$.multicast(newBlockByNumberSubject);
-exports.sendTransaction$ = function (signedData) {
-    return rxjs_1.Observable.fromPromise(CitaWeb3.sendTransaction(signedData));
-};
-exports.signTx$ = function (privKey, data) {
-    return rxjs_1.Observable.of(Web3Ins.eth.sign(privKey, data));
-};
+const rxjs_1 = require("@reactivex/rxjs");
+const cita_web3_plugin_1 = __importDefault(require("cita-web3-plugin"));
+class CITAObservables {
+    constructor({ server, interval = 1000, }) {
+        this.newBlockNumber$ = interval => rxjs_1.Observable.interval(interval || this.interval).switchMap(() => this.CitaWeb3.getBlockNumber());
+        this.blockByNumber$ = (blockNumber) => rxjs_1.Observable.fromPromise(this.CitaWeb3.getBlockByNumber({
+            quantity: blockNumber,
+            detailed: true,
+        }));
+        this.blockByHash$ = (blockHash) => rxjs_1.Observable.fromPromise(this.CitaWeb3.getBlockByHash({
+            hash: blockHash,
+            detailed: true,
+        }));
+        this.newBlockByNumber$ = interval => this.newBlockNumber$(interval || this.interval)
+            .distinct()
+            .switchMap(this.blockByNumber$);
+        this.peerCount$ = interval => rxjs_1.Observable.interval(interval || this.interval).switchMap(() => this.CitaWeb3.netPeerCount());
+        this.multicastedNewBlockByNumber$ = interval => {
+            const newBlockByNumberSubject = new rxjs_1.ReplaySubject(10);
+            this.newBlockByNumber$(interval || this.interval).multicast(newBlockByNumberSubject);
+            return newBlockByNumberSubject;
+        };
+        this.sendTransaction$ = signedData => rxjs_1.Observable.fromPromise(this.CitaWeb3.sendTransaction(signedData));
+        this.server = server;
+        this.interval = interval;
+        const Web3Ins = cita_web3_plugin_1.default({ server });
+        this.CitaWeb3 = Web3Ins.CITA;
+    }
+}
+exports.default = CITAObservables;
