b26cb91972f7b1d799001cf9a60f23221734cb36
Keith <keithwhisper@gmail.com>
2018-06-13 13:07:44 +0800

    update: update with plugin

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -14,44 +14,44 @@ export default class CITAObservables {
     });
     setServer: (server: string) => void;
     newBlockNumber: (interval?: number, observed?: boolean) => Observable<any>;
-    blockByNumber: (blockNumber: string) => Observable<JSONRPCResult>;
+    blockByNumber: (blockNumber: string | number) => Observable<JSONRPCResult>;
     blockByHash: (blockHash: string) => Observable<JSONRPCResult>;
     newBlockByNumber: (interval?: number, observed?: boolean) => Observable<JSONRPCResult>;
     peerCount: (interval?: number, observed?: boolean) => Observable<string | object | Transaction | JSONRPCError>;
     sendTransaction: (signedData: any) => Observable<string | object | Transaction | JSONRPCError>;
     blockHistory: (params: {
-        by: string;
+        by: string | number;
         count: number;
     }) => Observable<JSONRPCResult[]>;
     getLogs: ({ topics, fromBlock, }: {
         topics: string[];
-        fromBlock: string;
+        fromBlock: string | number;
     }) => Observable<string | object | Transaction | JSONRPCError>;
     ethCall: (params: {
         from: string;
         to: string;
         data: string;
-        blockNumber: string;
+        blockNumber: string | number;
     }) => Observable<string | object | Transaction | JSONRPCError>;
     getTransaction: (hash: string) => Observable<Transaction>;
     getTransactionCount: (params: {
         accountAddr: string;
-        blockNumber: string;
+        blockNumber: string | number;
     }) => Observable<string | object | Transaction | JSONRPCError>;
     getCode: (params: {
         contractAddr: string;
-        blockNumber: string;
+        blockNumber: string | number;
     }) => Observable<string | object | Transaction | JSONRPCError>;
     getAbi: (params: {
         contractAddr: string;
-        blockNumber: string;
+        blockNumber: string | number;
     }) => Observable<string | object | Transaction | JSONRPCError>;
     getTransactionProof: (hash: string) => Observable<string | object | Transaction | JSONRPCError>;
     metaData: ({ blockNumber }: {
-        blockNumber: string;
-    }) => Observable<string | object | Transaction | JSONRPCError>;
+        blockNumber: string | number;
+    }) => Observable<{}>;
     getBalance: ({ addr, blockNumber, }: {
         addr: string;
-        blockNumber: string;
+        blockNumber: string | number;
     }) => Observable<string | object | Transaction | JSONRPCError>;
 }
